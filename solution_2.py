f7c0aff9a02c03efbec4e0b20ed0049e5a7ab764
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-08-04 17:12:01 -0400

    Update solution_2.py

--- solution_2.py
+++ solution_2.py
@@ -48,3 +48,38 @@ for trial in range(num_trials):
 # calculate the average flips per trial
 avg_flips_per_trial = flips / num_trials
 print(f"\nThe average number of flips per trial is {avg_flips_per_trial}\n")
+
+
+
+# Alternative solution B
+
+import random
+
+def coin_flip():
+	"""Randomly return 'heads' or 'tail'."""
+	if random.randint(0, 1) == 0:
+		return "heads"
+	else:
+		return "tails"
+
+# keep track number of flips
+flips = 0
+# number of trial or number of flips
+num_trials = 10_000
+
+for trial in range(num_trials):
+	# flip the coin once and increment the flips tally by 1
+	first_flip = coin_flip()
+	flips += 1
+	
+	# continue flipping the coin and updating the tally until
+	# a different result is returned by coin_flips()
+	while coin_flip() == first_flip:
+		flips += 1
+	# increment the flip tally once more to account for the 
+	# final flip with a different result
+	flips += 1
+
+# calculate the average per flips
+avg_flips_per_trial = flips / num_trials 
+print(f"The average number of flips per trial is {avg_flips_per_trial}.")
