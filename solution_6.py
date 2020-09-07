c49090f824fd5ecc900ed05da97ff36f17581594
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-09-06 21:02:58 -0400

    Update solution_6.py

--- solution_6.py
+++ solution_6.py
@@ -1,30 +1,37 @@
-def newSort(my_array):
+# input: [-1, 3, 5, -1, 55, 23, -1, 43]
+# output: [-1, 55, 43, -1, 23, 5, -1, 3]
 
-    # created an empty list to hold
-    # the newly sorted values
+def new_sort(array):
+    # create an empty container to hold our 
+    # new sorted list
     container = []
-
-    # created a copied of passed in list
-    copy_arr = my_array[:]
-    for i in range(len(my_array)):
-
-        # check each items in orig list
-        # to see if if it a negative
-        if my_array[i] < 0:
-
-            # if it is append it to the container
-            container.append(my_array[i])
+    
+    # create a copy of the list
+    copy_array = array[:]
+    
+    # iterate throug our array
+    for i in range(len(array)):
+        
+        # check if the index value is 
+        # less than 0 
+        if array[i] < 0:
+            
+            # if so append it to the
+            # counter container
+            container.append(array[i])
+        
         else:
-
-            # take the max number since we want the 
-            # list to be in descending order
-            container.append(max(copy_arr))
-
-            # destroying the copy by removing each 
-            # item in the copied list
-            copy_arr.remove(max(copy_arr))
+            
+            # use the copy array take the
+            # max number and append it to container
+            container.append(max(copy_array))
+            
+            # remove the max value to copy array
+            copy_array.remove(max(copy_array))
+    
+    # return the value of the container
     return container
 
-my_array = [-1, 3, 5, -1, 55, 23, -1, 43]
+array = [-1, 3, 5, -1, 55, 23, -1, 43]
 
-print(newSort(my_array))
+print(new_sort(array))
