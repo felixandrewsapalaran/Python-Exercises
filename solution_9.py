f2fe6da17ddc25c3a189671837148028cd4df1a2
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-09-06 21:52:09 -0400

    Update solution_9.py

--- solution_9.py
+++ solution_9.py
@@ -1,18 +1,33 @@
-def sum_divisor(n):
-    sum = 0
-    x = 1
-
-    while x < n:
-        if n % x == 0:
-            sum += x
-        x += 1
+def sum_divisor(given_num):
     
-    # return  the sum of all divisors of 
-    # n , not including n
-    return sum
+    # keep track of the sum
+    sum = 0
+    
+    # counter
+    x = 1
+    
+    # keep looping as long as the counter is 
+    # less than the given number
+    while x < given_num:
+        # check if the number divide by x
+        # return a 0 remainder
+        if given_num % x == 0:
+            # if so take the value of counter 
+            # add it to our sum
+            sum += x
+        
+        # increment the counter by 1s
+        x  += 1
+    
+    # return total sum of all divisor 
+    # of given number not including the given number
+    return  sum
+
 
 print(sum_divisor(0)) # 0   Total = 0
 print(sum_divisor(3)) # Should sum of 1   Total = 1
 print(sum_divisor(36)) # Should sum of 1+2+3+4+6+9+12+18   Total = 55
-print(sum_divisor(102)) # Should sum of 2+3+6+17+34+51                  Total =  114
-
+print(sum_divisor(102)) # Should sum of 2+3+6+17+34+51            Total =  114
+            
+    
+    
