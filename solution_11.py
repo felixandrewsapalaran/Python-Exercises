6c35a2f2cfadc9f8d73f47db9e9cfbfd3fe0a343
Felix Andrew Sapalaran <40646937+felixandrewsapalaran@users.noreply.github.com>
2020-09-06 23:44:39 -0400

    Update solution_11.py

--- solution_11.py
+++ solution_11.py
@@ -1,9 +1,19 @@
-def factorial(n):
+def factorial(number):
+    # counter
     result = 1
-    for i in range(n):
+    
+    for i in range(number):
+        # example 4
+        # init result = 1
+        # 0 += 1 * 0
+        # 1 += 1 * 1
+        # result = 2
+        # 2 += 2 * 2
+        # result = 6
+        # 6 += 6 * 3
+        # result = 24
         result += result * i
     return result
 
-
-print(factorial(4)) # should return 24
-print(factorial(5)) # should return 120
+print(factorial(4)) # this returns 24
+print(factorial(5)) # this returns 120
